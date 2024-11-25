d3a3c2ff694015164aa40cc08ad6589471d641cd
Luciano Gasparin <lucianogasparin57@gmail.com>
2024-11-25 17:23:48 -0300

    keyboard reimplementado usando canvas

--- index.js
+++ index.js
@@ -1,16 +1,68 @@
 
-let abc = 'qwertyuiopasdfghjklzxcvbnm';
-for (let i = 0; i < abc.length; i++) {
+const canvas = document.getElementById('myCanvas');
+const ctx = canvas.getContext('2d');
 
-    if (i < abc.indexOf('a')) {
-        document.getElementById('firstLine').innerHTML += '<div class="keys" id="' + abc[i] + '"><h2>' + abc[i] + '</h2></div>';
-    } else if (i < abc.indexOf('z')) {
-        document.getElementById('secondLine').innerHTML += '<div class="keys" id="' + abc[i] + '"><h2>' + abc[i] + '</h2></div>';
-    } else {
-        document.getElementById('thirdLine').innerHTML += '<div class="keys" id="' + abc[i] + '"><h2>' + abc[i] + '</h2></div>';
-        
+// Declarando e definindo parâmetros
+const _wdt = 75;
+const _hgt = 75;
+const _horizontalGap = 5;
+const _verticalGap = 5;
+const abc = 'qwertyuiopasdfghjklzxcvbnm';
+
+const keysLine0 = [];
+const keysLine1 = [];
+const keysLine2 = [];
+const keysMatrix = [];
+
+// Atribuindo os elementos à cada uma das linhas do Keyboard
+function wrapLines(wrap0, wrap1, wrap2) {
+    for (let i = 0; i < abc.length; i++) {
+        let letter = {name: abc[i], width: _wdt, height: _hgt, x_position: undefined, y_position: undefined};
+        if (wrap0.includes(abc[i])) {
+            keysLine0.push(letter);
+        } else if (wrap1.includes(abc[i])) {
+            keysLine1.push(letter);
+        } else if (wrap2.includes(abc[i])) {
+            keysLine2.push(letter);
+        };
     };
+    keysMatrix.push(keysLine0, keysLine1, keysLine2);
 };
 
+// Chamando wrapLines e definindo as linhas de keys no Keyboard
+wrapLines('qwertyuiop', 'asdfghjkl', 'zxcvbnm');
 
-// '<div class="keys"><h2>' + abc[i] + '</h2></div>'
+// Espaçamento e centralização do Keyboard
+const keysColumn = keysMatrix.map(array => array[0])
+const totalHeight = keysColumn.reduce((sum, k) => sum + k.height, 0) + (keysMatrix.length - 1) * _verticalGap;
+var startY = (canvas.height - totalHeight) / 2;
+
+keysMatrix.forEach( keysLine => {
+    const totalWidth = keysLine.reduce((sum, k) => sum + k.width, 0) + (keysLine.length - 1) * _horizontalGap;
+    var startX = (canvas.width - totalWidth) / 2;
+    keysLine.forEach( k => {
+        // Definindo a centralização do texto do elemento
+        let centerX = startX + k.width / 2;
+        let centerY = startY + k.height / 2;
+
+        // Desenhar o retângulo das letras
+        ctx.strokeRect(startX, startY, k.width, k.height);
+    
+        // Adicionar o texto centralizado
+        ctx.fillStyle = "black";
+        ctx.font = '30px Arial'
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
+        ctx.fillText(k.name.toUpperCase(), centerX, centerY);
+        
+        // Armazenar posições X e Y do elemento
+        k['x_position'] = centerX
+        k['y_position'] = centerY
+
+        // Atualizar posição X para o próximo elemento
+        startX += k.width + _horizontalGap;
+    });
+    // Atualizar a posição Y para a próximo linha de elementos
+    startY += keysLine[0].height + _verticalGap;
+    
+});
---
+++ test.js
@@ -0,0 +1,11 @@
+const obj = {
+    x: 0, y: 1
+};
+const objL = [
+    {x: 'x', y: 'y', z: 'z'}
+]
+
+console.log('type of obj: ' + typeof(obj))
+console.log('type of objL: ' + typeof(objL))
+console.log(objL[0])
+
